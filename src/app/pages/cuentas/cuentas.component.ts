7414b2590d2f2c50bfb40e6c78913a31843dec8a
Victor Garcia <vgarcia@payall.com.ve>
2025-07-25 14:33:13 -0400

    ✨ Mostrar lista de cuentas

--- src/app/pages/cuentas/cuentas.component.ts
+++ src/app/pages/cuentas/cuentas.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
+import { CuentasService } from '../../services/cuentas.service';
+import { Cuenta } from '../../models/cuenta.model';
 
 @Component({
   selector: 'app-cuentas',
+  standalone: true,
   imports: [CommonModule, SearchBarComponent],
   templateUrl: './cuentas.component.html',
   styleUrl: './cuentas.component.scss'
 })
-export class CuentasComponent {
-  cuentas: any[] = [];
-  filteredCuentas: any[] = [];
+export class CuentasComponent implements OnInit {
+  cuentas: Cuenta[] = [];
+  filteredCuentas: Cuenta[] = [];
+
+  constructor(private cuentasService: CuentasService) {}
 
   ngOnInit(): void {
     this.loadCuentas();
   }
 
   loadCuentas(): void {
-    this.filteredCuentas = [...this.cuentas];
+    this.cuentasService.getCuentas().subscribe({
+      next: (data) => {
+        this.cuentas = data;
+        this.filteredCuentas = [...this.cuentas];
+      },
+      error: (error) => {
+        console.error('Error al cargar cuentas:', error);
+      }
+    });
   }
 
   onSearch(searchTerm: string): void {
