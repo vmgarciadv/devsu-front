acbf108c5b64ac141d5c85bea75c1aac8446f8b8
Victor Garcia <vgarcia@payall.com.ve>
2025-07-25 18:22:59 -0400

    ✨ Obtener todos los movimientos

--- src/app/pages/movimientos/movimientos.component.ts
+++ src/app/pages/movimientos/movimientos.component.ts
@@ -1,23 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from '../../components/shared/search-bar/search-bar.component';
+import { NotificationModalComponent } from '../../components/shared/notification-modal/notification-modal.component';
+import { MovimientosService } from '../../services/movimientos.service';
+import { NotificationService } from '../../services/notification.service';
+import { Movimiento } from '../../models/movimiento.model';
 
 @Component({
   selector: 'app-movimientos',
-  imports: [CommonModule, SearchBarComponent],
+  standalone: true,
+  imports: [CommonModule, SearchBarComponent, NotificationModalComponent],
   templateUrl: './movimientos.component.html',
   styleUrl: './movimientos.component.scss'
 })
-export class MovimientosComponent {
-  movimientos: any[] = [];
-  filteredMovimientos: any[] = [];
+export class MovimientosComponent implements OnInit {
+  movimientos: Movimiento[] = [];
+  filteredMovimientos: Movimiento[] = [];
+  notificationState$;
+
+  constructor(
+    private movimientosService: MovimientosService,
+    private notificationService: NotificationService
+  ) {
+    this.notificationState$ = this.notificationService.notification$;
+  }
 
   ngOnInit(): void {
     this.loadMovimientos();
   }
 
   loadMovimientos(): void {
-    this.filteredMovimientos = [...this.movimientos];
+    this.movimientosService.getMovimientos().subscribe({
+      next: (data) => {
+        this.movimientos = data;
+        this.filteredMovimientos = [...this.movimientos];
+      },
+      error: (error) => {
+        console.error('Error al cargar movimientos:', error);
+      }
+    });
   }
 
   onSearch(searchTerm: string): void {
@@ -26,5 +47,9 @@ export class MovimientosComponent {
 
   onNewMovimiento(): void {
     console.log('Crear nuevo movimiento');
+  }
+
+  closeNotification(): void {
+    this.notificationService.close();
   }
 }
